dbe15e3696d36e73207f9b4579214c6e6045d062
Muhammad Tahir Sultan <muhammad.tahir.sultan.official@gmail.com>
2025-12-27 23:51:12 +0500

    feat: implement initial authentication module with JWT and Passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,7 +5,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { AdminSetupService } from './admin-setup.service';
 import { JwtStrategy } from './jwt.strategy';
 import { User, UserSchema } from '../users/schemas/user.schema';
 
@@ -25,7 +24,7 @@ import { User, UserSchema } from '../users/schemas/user.schema';
         MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     ],
     controllers: [AuthController],
-    providers: [AuthService, JwtStrategy, AdminSetupService],
+    providers: [AuthService, JwtStrategy],
     exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule { }
